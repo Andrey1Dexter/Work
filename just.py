d6de60cb70cd1846650e64556b66f8728c2b4be3
Andrey1Dexter <dexter.andrey.at@gmail.com>
2020-09-30 11:56:35 +0300

    Homework

--- just.py
+++ just.py
@@ -1,5 +1,3 @@
-my_str = 'My name is Andrey !'
-
-for symbol in my_str:
-        if not symbol.isupper() and symbol.isalpha():
-                print(symbol)
+my_list = ['a', 'b', 'c', 'd', 'e', 'f']
+my_str = ''.join(my_list[::2])
+print(my_str)
